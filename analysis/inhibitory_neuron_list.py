6fcc4412bb9df26efc355acd0d4039df49b01c5c
Stephen Jonany <sjonany@gmail.com>
2020-03-20 15:45:35 -0700

    Add missing import that for some reason pycharm bypassed

--- analysis/inhibitory_neuron_list.py
+++ analysis/inhibitory_neuron_list.py
@@ -2,6 +2,7 @@
 List of inhibitory neurons.
 """
 import numpy as np
+import project_path  # pylint: disable=unused-import
 
 from model.data_accessor import get_data_file_abs_path
 from model.neuron_metadata import NeuronMetadataCollection
