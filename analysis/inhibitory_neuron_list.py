c64519860207305f5f8f16d37ea7c058a460d1f4
Stephen Jonany <sjonany@gmail.com>
2020-03-20 15:41:04 -0700

    Add a script to list names of inhibitory neurons.

---
+++ analysis/inhibitory_neuron_list.py
@@ -0,0 +1,23 @@
+"""
+List of inhibitory neurons.
+"""
+import numpy as np
+
+from model.data_accessor import get_data_file_abs_path
+from model.neuron_metadata import NeuronMetadataCollection
+
+def main():
+    """
+    :return:
+    """
+    neuron_metadata_collection = NeuronMetadataCollection.load_from_chem_json(\
+        get_data_file_abs_path('chem.json'))
+    inhibitory_neuron_ids = np.argwhere(\
+        np.load(get_data_file_abs_path('emask.npy')).flatten()).flatten()
+    inhibitory_neuron_names = []
+    for neuron_id in inhibitory_neuron_ids:
+        metadata = neuron_metadata_collection.get_metadata(neuron_id)
+        inhibitory_neuron_names.append(metadata.name)
+    print(sorted(inhibitory_neuron_names))
+
+main()
